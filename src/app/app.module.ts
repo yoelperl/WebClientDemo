a5e0d7bb73beca2c18d15c0aa999f93cf97a0a51
yoelp <yperl@magicsoftware.com>
2018-03-11 16:54:13 +0200

    merge stuff from older, working, WebClientDemo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,34 @@ import {MagicModule} from "./magic/magic.core.module";
 import {AppComponent} from "./app.component";
 import {DynamicModule} from "ng-dynamic-component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule, MatSelectModule, MatToolbarModule
+} from "@angular/material";
 import {ComponentsList} from "./ComponentList";
 import {ThemeModule} from "./magic/src/ui/theme/theme.module";
 import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {CalendarModule} from 'primeng/components/calendar/calendar';
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
+// import {WMatTimePickerComponent} from "./components/TimePicker/time-control/w-mat-timepicker.component";
+// import {WClockComponent, WTimeDialogComponent} from "./components/TimePicker";
 
 const comps = ComponentsList.getAllComponents();
 
 @NgModule({
-   declarations: [AppComponent, ...comps],
+   declarations: [AppComponent, ...comps,   // timepicker
+    // WMatTimePickerComponent,
+    // WTimeDialogComponent,
+    // WClockComponent,
+     ],
    imports     : [
       BrowserModule,
       FormsModule,
@@ -26,13 +45,28 @@ const comps = ComponentsList.getAllComponents();
       DynamicModule.withComponents(comps),
       MagicModule.forRoot(),
       CalendarModule,
-
-      ThemeModule,
+   // Angular Material
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatToolbarModule,
+    MatSelectModule,
+    ThemeModule,
    ],
    exports     : [FormsModule, ReactiveFormsModule, InfiniteScrollModule],
    providers   : [],
 
-   bootstrap: [AppComponent]
+   bootstrap: [AppComponent],
+   // entryComponents:[WTimeDialogComponent]
 })
 export class AppModule
 {
